c8d46e41df5882ff97805d41e098646f5b3d764e
Michael Möhler <michael.moehler@rhein-spree.com>
2024-06-24 09:02:36 +0200

    Add basic application

--- Program.fs
+++ Program.fs
@@ -1,2 +1,57 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿module App
+
+open Feliz
+open Feliz.UseElmish
+open Elmish
+open Elmish.Toastr
+
+module Counter =
+    type Model =
+        {
+            count : int
+        }
+    let init() =
+        {
+            count = 0
+        }
+        , Cmd.none
+    type Msg =
+        | Increment
+        | Decrement
+    let update msg model =
+        
+        let showToastCmd (msg: string) =
+            Toastr.message msg
+            |> Toastr.position TopRight
+            |> Toastr.timeout 3000
+            |> Toastr.withProgressBar
+            |> Toastr.hideEasing Easing.Swing
+            |> Toastr.success
+        
+        match msg with
+        | Increment ->
+            { model with
+                count = model.count + 1
+            }
+            , showToastCmd $"Counter: {model.count}"
+        | Decrement ->
+            { model with
+                count = model.count - 1
+            }
+            , showToastCmd $"Counter: {model.count}"
+            
+    [<ReactComponent>]
+    let View () =
+        let _, dispatch = React.useElmish(init, update, [| |])
+        Html.div [
+            Html.button [
+                prop.text "Decrement"
+                prop.onClick (fun _ -> Decrement |> dispatch)
+            ]
+            Html.button [
+                prop.text "Increment"
+                prop.onClick (fun _ -> Increment |> dispatch)
+            ]
+        ]
+
+(ReactDOM.createRoot (Browser.Dom.document.getElementById "root")).render (Counter.View ())
---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+﻿import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+    clearScreen: false,
+    server: {
+        watch: {
+            ignored: [
+                "**/*.fs" // Don't watch F# files
+            ]
+        }
+    }
+})
